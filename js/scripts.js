1a05a84ea42ce9536b222beba8925b16888f297d
Mikhail Petrov and Maggie Summers <mikhail@>
2018-01-22 14:04:16 -0800

    js functionality for adding list items to array and displaying them

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,26 @@ function Task(item, isComplete) {
 };
 
 $(document).ready(function(){
+
   var isComplete = false;
+  var allTasks = [];
+
   $("form#new-list").submit(function(event) {
+
     event.preventDefault();
     var newItem = $("#to-do").val();
-
     var newTask = new Task(newItem, isComplete);
+    allTasks.push(newTask);
 
     $("#checklist").show();
-    $("ul#list").append("<li class='item'>" + newTask.item + "</li>");
 
-    $(".item").last().click(function() {
-      $(".item").fadeOut();
-    });
+    for(index = allTasks.length; index <= allTasks.length; index +=1) {
+      $("ul#list").append("<li class='item'>" + allTasks[index-1].item + "</li>");
+    };
+
+    // $(".item").last().click(function() {
+    //   $(".item").fadeOut();
+    // });
 
   });
 
